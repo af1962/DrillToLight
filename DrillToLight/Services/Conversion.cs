0456a5246d960c9c3919df656e3cc40f703c831a
Farid <f-adda@hotmail.fr>
2023-06-02 10:33:25 +0200

    Factorisation du code de conversion

--- DrillToLight/Services/Conversion.cs
+++ DrillToLight/Services/Conversion.cs
@@ -1,50 +1,37 @@
 ﻿using DrillToLight.Interfaces;
 using System.Collections.ObjectModel;
+using System.Diagnostics.Eventing.Reader;
 
 namespace DrillToLight.Services
 {
     internal class Conversion : IConversion
     {
         // Création d'une nouvelle collection sinon les modifications portent aussi sur GcodeOriginal
-        ObservableCollection<string> conversion;
+        ObservableCollection<string>? conversion;
         public ObservableCollection<string> GetConvertir(ObservableCollection<string> gcodeDrill)
         {
-            conversion = new ObservableCollection<string>(gcodeDrill);
+            conversion = new ObservableCollection<string>();
 
-            // Effacement des 10 preimères lignes
-            for (int i = 0; i <= 9; i++)
+            for (int i = 10; i <= gcodeDrill.Count - 5; i++)
             {
-                conversion.RemoveAt(0);
+
+                if (!gcodeDrill[i].Contains("-") && !gcodeDrill[i].Contains("Z0") && !gcodeDrill[i].Contains("Z2"))
+                {
+                    conversion.Add(gcodeDrill[i]);
+                }
+
+                if (gcodeDrill[i].Contains("Z0"))
+                {
+                    conversion.Add("M03 S1000");
+                }
+
+                if (gcodeDrill[i].Contains("Z2"))
+                {
+                    conversion.Add("M03 S0");
+                }                           
             }
 
-            // Effacement des 4 dernières lignes
-            for (int i = 0; i <= 3; i++)
-            {
-                conversion.RemoveAt(conversion.Count - 1);
-            }
-
-            // Ajout d'une ligne à la fin pour arrêter le faisceau
             conversion.Add("M03 S0");
-
-            // Remplacement Drill pour laser
-            for (int i = conversion.Count - 1; i >= 0; i--)
-            {
-                if (conversion[i].Contains("Z0"))
-                {
-                    conversion[i] = "M03 S1000";
-                }
-
-                if (conversion[i].Contains("Z2"))
-                {
-                    conversion[i] = "M03 S0";
-                }
-
-                if (conversion[i].Contains("-"))
-                {
-                    conversion.RemoveAt(i);
-                }
-
-            }
 
             return conversion;
         }
