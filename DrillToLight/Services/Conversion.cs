179f5e3991d93100bb72fb2fa77c15cf98466944
ADDA <f-adda@hotmail.fr>
2024-02-23 13:04:59 +0100

    Modif pour compatibilité avec Candle

--- DrillToLight/Services/Conversion.cs
+++ DrillToLight/Services/Conversion.cs
@@ -6,33 +6,36 @@
         public ObservableCollection<string> GetConvertir(ObservableCollection<string> gcodeDrill)
         {
             conversion = new ObservableCollection<string>();
-            string recherche = "";
+            string str = "";
             int k = 0;
-            while (recherche !="G00 Z2")
+            while (str != "G00 Z2")
             {
-                recherche = gcodeDrill[k];
+                str = gcodeDrill[k];
                 k++;
             }
 
-            for (int i = k-1; i < gcodeDrill.Count - 4; i++)
+            for (int i = k - 1; i < gcodeDrill.Count - 4; i++)
             {
 
-                if (!gcodeDrill[i].Contains('-') && !gcodeDrill[i].Contains("Z0") && !gcodeDrill[i].Contains("Z2"))
+                if (gcodeDrill[i].Contains('Z'))
+                {
+                    if (gcodeDrill[i].Contains("Z0"))
+                    {
+                        conversion.Add("G0 Z0 M03 S1000");
+                    }
+                    else
+                    {
+                        if (!gcodeDrill[i].Contains('-'))
+                        {
+                            conversion.Add("G0 Z0 M03 S0");
+                        }
+                    }
+                }
+                else
                 {
                     conversion.Add(gcodeDrill[i]);
                 }
-
-                if (gcodeDrill[i].Contains("Z0"))
-                {
-                    conversion.Add("G0 Z0 M03 S1000");
-                }
-
-                if (gcodeDrill[i].Contains("Z2"))
-                {
-                    conversion.Add("G0 Z0 M03 S0");
-                }
             }
-
             conversion.Add("M03 S0");
 
             return conversion;
