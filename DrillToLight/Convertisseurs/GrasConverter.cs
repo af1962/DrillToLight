c57a74a4e86f8a17b03b7ed3b156c1ebb6309aee
ADDA <f-adda@hotmail.fr>
2024-07-07 11:00:50 +0200

    Police gras et couleur sur vitesse et puissance

--- DrillToLight/Convertisseurs/GrasConverter.cs
+++ DrillToLight/Convertisseurs/GrasConverter.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace DrillToLight.Convertisseurs;
 
-internal class VisibleConverter : IValueConverter
+internal class GrasConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return !string.IsNullOrEmpty((string)value) ? Visibility.Visible : Visibility.Hidden;
+        string? ligne = value as string;
+        if (ligne.Contains('F') || ligne.Contains('S'))
+        {
+            return "Bold";
+        }
+        else
+        {
+            return "Normal";
+        }
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
